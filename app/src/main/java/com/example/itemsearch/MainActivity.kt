a3e982b31ebebdf572d8435fe8b8fbf5cf8b0bc9
Santiago Joel Cerdán <santyjoell@gmail.com>
2021-09-01 21:00:27 -0300

    Items obtains

--- app/src/main/java/com/example/itemsearch/MainActivity.kt
+++ app/src/main/java/com/example/itemsearch/MainActivity.kt
@@ -5,24 +5,14 @@ import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
-import com.example.itemsearch.data.APIservice
 import com.example.itemsearch.databinding.ActivityMainBinding
-import com.example.itemsearch.model.ItemSearch
-import com.example.itemsearch.model.Result
 import com.example.itemsearch.ui.ItemAdapter
 import com.google.gson.Gson
-import com.google.gson.JsonArray
-import com.google.gson.JsonObject
-import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -55,41 +45,25 @@ class MainActivity : AppCompatActivity() {
 
         val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, URL, null,
             { response ->
-                val gson = Gson()
-                val rta = response.getJSONArray("results")
-                for (i in 0 until rta.length()) {
-                    val jsonObject = rta.getJSONObject(i)
-                    val titulo = jsonObject.get("title")
-                    val thumbnail = jsonObject.get("thumbnail")
-                    Log.e(titulo.toString(), thumbnail.toString())
+
+                GlobalScope.launch(Dispatchers.IO) {
+                    val rta = response.getJSONArray("results")
+
+                    runOnUiThread {
+                        for (i in 0 until rta.length()) {
+                            val jsonObject = rta.getJSONObject(i)
+                            val titulo = jsonObject.get("title")
+                            val thumbnail = jsonObject.get("thumbnail")
+                            Log.e(titulo.toString(), thumbnail.toString())
+                        }
+                    }
                 }
             },
             { error ->
                 Log.e("ERROR", error.toString())
             })
-
         queue.add(jsonObjectRequest)
-
-
-//        val api = Retrofit.Builder()
-//            .baseUrl("https://api.mercadolibre.com/sites/MLA/")
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build()
-//            .create(APIservice::class.java)
-//
-//        GlobalScope.launch(Dispatchers.IO) {
-//            val response = api.getSearchItem(url)
-//            val rta = response
-//
-//            val gson = Gson()
-//            val dto = gson.toJson(rta)
-//
-//            runOnUiThread {
-//                Log.e("VER", dto)
-//            }
-//        }
     }
-
 }
 
 
